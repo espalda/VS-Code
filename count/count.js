bcc7e58259c39a68a9e5c742c705ffd3c9239cb3
espalda <espalda@naver.com>
2020-02-07 17:05:35 +0900

    Commit

--- count/count.js
+++ count/count.js
@@ -1,36 +1,44 @@
-//ar focus = $('input').eq(0).focus();   //original focus only one
-var cnt1= $('.cntMain').eq(0);
-console.log(cnt1);
+$(document).ready(function(){
+    var focus = $('input').eq(0).focus();   //original focus only one
+    //focus on input
+    $('input').focus(function(){
+        focus = $(this);
+    });
 
-//focus on input
-$('input').focus(function(){
-    focus = $(this);
-});
+    //insert number
+    $('.btn').click(function(){
+        var val = $(this).attr("value");
+        var str = focus.val();
+        res = focus.val(str += val);
+    })
 
-//insert number
-$('.btn').click(function(){
-    var val = $(this).attr("value");
-    var str = focus.val();
-    focus.val(str += val);
-})
+    //all number clear
+    $('.clear').click(function(){
+        $(focus).val('');
+    })
 
-//length limit
-$('.btn').click(function(){
-    var cnt = focus.val().length;
-    if(cnt == 11) {
-        alert("10자 까지 입력할수 있습니다.");
-        var limit = focus.val().slice(0, 10);
-        focus.val(limit);
-    }
-})
+    //only one number delete
+    $('.del').click(function(){
+        var str = focus.val();
+        focus.val(str.substring(0, str.length-1));
+    })
 
-//all number clear
-$('.clear').click(function(){
-    $(focus).val('');
-})
+    $('.symbol').click(function(){
+        var sym = $(this).text();
+        if(sym == '-'){
+            $(this).text(sym.replace('-', '+'));
+            //console.log(1);
+        }else{
+            $(this).text(sym.replace('+', '-'));
+            //console.log(2);
+        }
+    })
 
-//only one number delete
-$('.del').click(function(){
-    var str = focus.val();
-    focus.val(str.substring(0, str.length-1));
-})
+    //온도 오차 구하기
+    //클릭한 버튼의 value값을 gap의 value 값으로 가져온다.
+    //01 . input 창의 값을 가져온다
+    //02. .symbol 이 - 일때 input 창의 값
+    //03. 맨앞에 - 와 맨뒤에 %를 붙인다.
+    //04. -일때 클릭하면 +로 +일때 클릭하면 -로 변환
+
+}) //document.ready
