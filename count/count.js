78d915d62284499c6e1f67278fb6fddc1ee9be87
espalda <espalda@naver.com>
2020-02-07 17:37:11 +0900

    push

--- count/count.js
+++ count/count.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-    var focus = $('input').eq(0).focus();   //original focus only one
+
+    var focus = $('input').eq(0).focus();
+    var res = '';
+
     //focus on input
     $('input').focus(function(){
         focus = $(this);
@@ -10,8 +13,10 @@ $(document).ready(function(){
         var val = $(this).attr("value");
         var str = focus.val();
         res = focus.val(str += val);
+        console.log('str= ' + str);
+        console.log('res= ' + res.val());
     })
-
+    
     //all number clear
     $('.clear').click(function(){
         $(focus).val('');
@@ -25,20 +30,30 @@ $(document).ready(function(){
 
     $('.symbol').click(function(){
         var sym = $(this).text();
-        if(sym == '-'){
-            $(this).text(sym.replace('-', '+'));
+        if(sym == '+'){
+            $(this).text(sym.replace('+', '-'));
             //console.log(1);
         }else{
-            $(this).text(sym.replace('+', '-'));
+            $(this).text(sym.replace('-', '+'));
             //console.log(2);
         }
     })
 
+    $('.temp .btn').click(function(){
+        
+    })
     //온도 오차 구하기
-    //클릭한 버튼의 value값을 gap의 value 값으로 가져온다.
+    //클릭한 버튼의 value값을 gap의 value값으로 가져온다.
     //01 . input 창의 값을 가져온다
     //02. .symbol 이 - 일때 input 창의 값
     //03. 맨앞에 - 와 맨뒤에 %를 붙인다.
     //04. -일때 클릭하면 +로 +일때 클릭하면 -로 변환
 
 }) //document.ready
+
+//온도 값 퍼센트로 바꾸기
+//길이제한 10자까지
+//input 창에 숫자만 입력 가능하게
+//화면 설계서 보기
+//clearfix 정리하기
+//화면 width 어디서 오차가 생기는지 정리
